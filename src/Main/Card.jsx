3f790915eefdc58c3fe75f855e121e3d41a12748
NaziaBeaghum <nazia.mk2@gmail.com>
2024-07-28 20:46:38 +0530

    second commit

--- src/Main/Card.jsx
+++ src/Main/Card.jsx
@@ -5,12 +5,12 @@ import { Link } from 'react-router-dom'
 const Card = ({item}) => {
   return (
     <div className="card_container">
-        <Link to="/Single"><img src={item.image} alt="" className='card_image'/></Link>
-       <p className=' card_title roboto-medium'> {item.title}</p>
-        <p>{item.description}</p>
-        <p>Date<span>{item.date}</span></p>
-        <p>Location<span>{item.location}</span></p>
-        <p className='roboto-medium'>Price<span>${item.price}</span></p>
+        <Link to={`/Single/${item.id}`}><img src={item.image} alt="" className='card_image'/></Link>
+       <p className='card_title roboto-medium'><Link to={`/Single/${item.id}`}>{item.title}</Link></p>
+        <p className='roboto-regular'>{item.description}</p>
+        <p className='roboto-medium' >Date:<span className='roboto-light'>{item.date}</span></p>
+        <p className='roboto-medium'>Location:<span className='roboto-light'>{item.location}</span></p>
+        <p className='roboto-medium'>Price:<span className='roboto-light'>${item.price}</span></p>
     </div>
   )
 }
--- src/Main/Single.jsx
+++ src/Main/Single.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+import { useState } from 'react'
+import './Card.css'
 
 const Single = () => {
+  const{id}=useParams()
+  const[detail,setDetail]=useState([])
+  useEffect(()=>{axios.get(`https://669f704cb132e2c136fdd9a0.mockapi.io/api/v1/retreats/${id}`)
+               .then((res)=>setDetail(res.data))},[])
+               console.log(detail)
   return (
-    <div>Single</div>
-  )
+   <div className="main_container">
+    <div className="sub_container">
+      <img src={detail.image}  alt="" className='detail_img'/>
+      <p className='roboto-bold'>{detail.title}</p>
+     <ul>
+      <li><p className='roboto-regular' >Duration:{detail.duration} hrs</p></li>
+      <li><p className='roboto-regular'>Location:{detail.location}</p></li>
+      <li><p className='roboto-regular'>Price:${detail.price}</p></li>
+      <li><p className='roboto-regular'>{detail.description}</p></li>
+      <p className='roboto-regular'>Main Focus on these three scenarios:</p>
+     </ul >
+     <ul className='list_style'>
+      {detail?.tag?.map((item)=>(<li ><p className='roboto-regular'>{item}</p></li>))} 
+      </ul>
+      </div> 
+   </div>
+  ) 
 }
 
 export default Single
+
+condition
+: 
+"Stress Relief"
+date
+: 
+1692921600
+description
+: 
+"A weekend retreat focused on yoga and meditation to relieve stress."
+duration
+: 
+3
+id
+: 
+"1"
+image
+: 
+"https://cdn.midjourney.com/a287f9bc-d0fb-4e78-a0fa-e8136d3c408a/0_0.jpeg"
+location
+: 
+"Goa"
+price
+: 
+200
+tag
+: 
+(3) ['relaxation', 'meditation', 'weekend']
+title
+: 
+"Yoga for Stress Relief"
+type
+: 
+"Signature"
